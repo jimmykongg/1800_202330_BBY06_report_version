42b1a1c5d1b2767a4f1ba5df670a469ac7260c90
Tung Kiu Kong <jimmyatwork368@gmail.com>
2023-11-13 20:34:08 -0800

    Add new feature to profile.html. Now the user can update their profiles by clicking 'Edit your profile'button. Users' personal information is written dynamically to Firestore User collection.

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,97 +1,157 @@
+// Developed by Jimmy
+// Read data
+
 /* Insert name */
 function insertNameFromFirestore() {
-  // Check if the user is logged in:
-  firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-          console.log(user.uid); // Let's know who the logged-in user is by logging their UID
-          currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
-          currentUser.get().then(userDoc => {
-              // Get the user name
-              var userName = userDoc.data().name;
-              console.log(userName);
-              //$("#name-goes-here").text(userName); // jQuery
-              document.getElementById("name-goes-here").innerText = userName;
-          })
-      } else {
-          console.log("No user is logged in."); // Log a message when no user is logged in
-      }
-  })
+    // Check if the user is logged in:
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log(user.uid); // Let's know who the logged-in user is by logging their UID
+            currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
+            currentUser.get().then(userDoc => {
+                // Get the user name
+                var userName = userDoc.data().name;
+                console.log(userName);
+                //$("#name-goes-here").text(userName); // jQuery
+                document.getElementById("name-goes-here").innerText = userName;
+            })
+        } else {
+            console.log("No user is logged in."); // Log a message when no user is logged in
+        }
+    })
 }
 
 insertNameFromFirestore();
 
 /* Insert email */
 function insertEmailFromFirestore() {
-  firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-          console.log(user.uid); 
-          currentUser = db.collection("users").doc(user.uid); 
-          currentUser.get().then(userDoc => {
-              var userName = userDoc.data().email;
-              console.log(userName);
-              document.getElementById("email-goes-here").innerText = userName;
-          })
-      } else {
-          console.log("No user is logged in.");
-      }
-  })
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log(user.uid);
+            currentUser = db.collection("users").doc(user.uid);
+            currentUser.get().then(userDoc => {
+                var userName = userDoc.data().email;
+                console.log(userName);
+                document.getElementById("email-goes-here").innerText = userName;
+            })
+        } else {
+            console.log("No user is logged in.");
+        }
+    })
 }
 
 insertEmailFromFirestore();
 
 /* Insert country */
 function insertCountryFromFirestore() {
-  firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-          console.log(user.uid); 
-          currentUser = db.collection("users").doc(user.uid); 
-          currentUser.get().then(userDoc => {
-              var userName = userDoc.data().country;
-              console.log(userName);
-              document.getElementById("country-goes-here").innerText = userName;
-          })
-      } else {
-          console.log("No user is logged in.");
-      }
-  })
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log(user.uid);
+            currentUser = db.collection("users").doc(user.uid);
+            currentUser.get().then(userDoc => {
+                var userName = userDoc.data().country;
+                console.log(userName);
+                document.getElementById("country-goes-here").innerText = userName;
+            })
+        } else {
+            console.log("No user is logged in.");
+        }
+    })
 }
 
 insertCountryFromFirestore();
 
 /* Insert email */
 function insertLocationFromFirestore() {
-  firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-          console.log(user.uid); 
-          currentUser = db.collection("users").doc(user.uid); 
-          currentUser.get().then(userDoc => {
-              var userName = userDoc.data().recentLocation;
-              console.log(userName);
-              document.getElementById("location-goes-here").innerText = userName;
-          })
-      } else {
-          console.log("No user is logged in.");
-      }
-  })
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log(user.uid);
+            currentUser = db.collection("users").doc(user.uid);
+            currentUser.get().then(userDoc => {
+                var userName = userDoc.data().recentLocation;
+                console.log(userName);
+                document.getElementById("location-goes-here").innerText = userName;
+            })
+        } else {
+            console.log("No user is logged in.");
+        }
+    })
 }
 
 insertLocationFromFirestore();
 
 /* Insert counter */
 function insertCounterFromFirestore() {
-  firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-          console.log(user.uid); 
-          currentUser = db.collection("users").doc(user.uid); 
-          currentUser.get().then(userDoc => {
-              var userName = userDoc.data().recycleCounter;
-              console.log(userName);
-              document.getElementById("counter-goes-here").innerText = userName;
-          })
-      } else {
-          console.log("No user is logged in.");
-      }
-  })
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log(user.uid);
+            currentUser = db.collection("users").doc(user.uid);
+            currentUser.get().then(userDoc => {
+                var userName = userDoc.data().recycleCounter;
+                console.log(userName);
+                document.getElementById("counter-goes-here").innerText = userName;
+            })
+        } else {
+            console.log("No user is logged in.");
+        }
+    })
 }
 
 insertCounterFromFirestore();
+
+var currentUser; 
+            
+function populateUserInfo() {
+            firebase.auth().onAuthStateChanged(user => {
+                // Check if user is signed in:
+                if (user) {
+
+                    //go to the correct user document by referencing to the user uid
+                    currentUser = db.collection("users").doc(user.uid)
+                    //get the document for current user.
+                    currentUser.get()
+                        .then(userDoc => {
+                            //get the data fields of the user
+                            var userName = userDoc.data().name;
+                            var userEmail = userDoc.data().email;
+                            var userCountry = userDoc.data().country;
+
+                            //if the data fields are not empty, then write them in to the form.
+                            if (userName != null) {
+                                document.getElementById("nameInput").value = userName;
+                            }
+                            if (userEmail != null) {
+                                document.getElementById("emailInput").value = userEmail;
+                            }
+                            if (userCountry != null) {
+                                document.getElementById("countryInput").value = userCountry;
+                            }
+                        })
+                } else {
+                    // No user is signed in.
+                    console.log ("No user is signed in");
+                }
+            });
+        }
+
+//call the function to run it 
+populateUserInfo();
+
+// Save User info from profile editing modal
+function saveUserInfo() {
+
+    // Get user entered values
+    userName = document.getElementById('nameInput').value;      
+    userEmail = document.getElementById('emailInput').value;     
+    userCountry = document.getElementById('countryInput').value;  
+
+    // Update user's document in Firestore
+    currentUser.update({
+        name: userName,
+        email: userEmail,
+        country: userCountry
+    })
+    .then(() => {
+        console.log("Document successfully updated!");
+    })
+}
