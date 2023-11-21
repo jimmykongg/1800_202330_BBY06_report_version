89e6136fa0d1be48ac2a91513c2a7f2d1af41573
Leo Zheng <leozheng0901@gmail.com>
2023-11-21 10:27:26 -0800

    Fixed a bug where the distance would not show in the search.html file

--- scripts/search.js
+++ scripts/search.js
@@ -156,41 +156,4 @@ function getGeolocation() {
     }
 }
 
-function placeGeocoder() {
-    // TO MAKE THE MAP APPEAR YOU MUST
-    // ADD YOUR ACCESS TOKEN FROM
-    // https://account.mapbox.com
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYWRhbWNoZW4zIiwiYSI6ImNsMGZyNWRtZzB2angzanBjcHVkNTQ2YncifQ.fTdfEXaQ70WoIFLZ2QaRmQ';
-    const geocoder = new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        types: 'country,region,place,postcode,locality,neighborhood,address'
-    });
-
-    geocoder.addTo('#geocoder');
-
-    // Get the geocoder results container.
-    const results = document.getElementById('result');
-
-    // Add geocoder result to container.
-    geocoder.on('result', (e) => {
-        var jsondata = JSON.stringify(e.result, null, 2);
-        var data = JSON.parse(jsondata);
-        console.log(data);
-        var place_name = data["place_name"];
-        var place_coord = data["geometry"]["coordinates"];
-        results.innerText = place_name + " " + place_coord;  //show it on dom, debug
-
-        //You can save this into local storage for now.  
-        //When it is time to Submit the Post you can get the data from Local Storage
-        localStorage.setItem("place_name", place_name);
-        localStorage.setItem("place_coord", place_coord);
-    });
-
-    // Clear results container when search is cleared.
-    geocoder.on('clear', () => {
-        results.innerText = '';
-    });
-}
-placeGeocoder();
-
 getGeolocation();
