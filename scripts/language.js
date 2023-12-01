a697f3de3c28048a8d85131e379acc7164e25e26
Tung Kiu Kong <jimmyatwork368@gmail.com>
2023-12-01 01:17:21 -0800

    Finish development of language page. Now users can select their language preferences out of five languages. Modify language.html, footer.html, language.js.

--- scripts/language.js
+++ scripts/language.js
@@ -8,36 +8,44 @@
 // (2) changes languages of the navbar and welcome message 
 // on the main page
 //----------------------------------------------------------
-function switchlanguages() {
+function switchLanguage(targetLanguage) {
   // Get all the language switches
   const languageSwitches = document.querySelectorAll('.language-switch');
+  const switchElement = document.querySelector(`.${targetLanguage}`);
 
-  // Add click event listener to each switch
-  languageSwitches.forEach(switchElement => {
-    switchElement.addEventListener('click', function () {
-      // target switch cannot be unchecked
-      switchElement.checked = true;
-      
-      // Uncheck all other switches
-      languageSwitches.forEach(otherSwitch => {
-        if (otherSwitch !== switchElement) {
-          otherSwitch.checked = false;
-        }
+  // current target switch is always checked until another target switch is selected
+  switchElement.checked = true;
+
+  // Uncheck all other switches
+  languageSwitches.forEach(otherSwitch => {
+    if (otherSwitch !== switchElement) {
+      otherSwitch.checked = false;
+    }
+  });
+
+  // Update languages
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      db.collection("users").doc(user.uid).get().then(userDoc => {
+        let currentUser = db.collection("users").doc(user.uid);
+        currentUser.update({
+          language: targetLanguage
+        }).then(() => {
+          console.log("User's language preference is successfully updated!");
+        })
+
+        db.collection("languages").doc(targetLanguage).onSnapshot((doc) => {
+          // document.getElementById("welcome-message").innerHTML = doc.data().welcome_message;
+          document.getElementById("language-header").innerHTML = doc.data().header;
+          document.getElementById("home").innerHTML = doc.data().home;
+          document.getElementById("search").innerHTML = doc.data().search;
+          document.getElementById("fact").innerHTML = doc.data().fact;
+          document.getElementById("account").innerHTML = doc.data().account;
+        })
       });
-
-      //
-      switch(switchElement.getAttribute("id")) {
-        case "flexSwitchCheckChinese":
-          document.querySelector(".language-title").innerHTML = "語言設定"
-          document.querySelector(".icon-text-home").innerHTML = "主頁";
-          document.querySelector(".icon-text-search").innerHTML = "搜尋"
-          document.querySelector(".icon-text-facts").innerHTML = "冷知識";
-          document.querySelector(".icon-text-account").innerHTML = "帳戶"; 
-          break;
-      }
-    });
+    } else {
+      console.log("No user is signed in");
+    }
   });
 }
-
-switchlanguages();
 
